a89c96ef664466ef0acf4feae237276638f1b30e
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-24 20:29:40 +0200

    new things

---
+++ TicTacToe/Helper/FactoryMethod/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Helper.FactoryMethod
+{
+    public abstract class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public virtual Shape CreateShape()
+        {
+            return this;
+        }
+    }
+}
---
+++ TicTacToe/Models/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TicTacToe.Helper.FactoryMethod;
+
+namespace TicTacToe.Models
+{
+    public class Circle :Shape
+    {
+        public Line Line { get; set; }
+        public string Color { get; set; }
+        public override Shape CreateShape()
+        {
+            Line = new Line(0,0);
+            Color = "grey";
+            return base.CreateShape();  
+        }
+    }
+}
